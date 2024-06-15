2813b7cb5826a5bfa2ba27cc2835e85c4d6028c7
macastil <gmacastil@gmail.com>
2024-06-15 09:56:05 -0500

    ignore

--- obj/Debug/net8.0/api-test-sabado.AssemblyInfo.cs
+++ obj/Debug/net8.0/api-test-sabado.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api-test-sabado")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5ccc891e00c1ab088cba3a90c1acdf140e1043c")]
 [assembly: System.Reflection.AssemblyProductAttribute("api-test-sabado")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api-test-sabado")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
